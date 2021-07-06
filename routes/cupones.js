1bc9bd3a42e7b9cc6855dba68503ea65f2748000
DESIT-DEV <desit.dev@gmail.com>
2021-07-05 22:17:22 -0600

    mando ya agregando cupones

--- routes/cupones.js
+++ routes/cupones.js
@@ -2,12 +2,28 @@ const express = require('express');
 const router = express.Router();
 const pool= require('../config/database');
 
-router.get('/', function (req, res) {
-    res.render('cupones');
+router.get('/', async (req, res)=> {
+  const cupones= await pool.query('SELECT * FROM cupones');
+    res.render('cupones',{cupones});
   });
 
-router.get('/add', function (req, res) {
+
+
+router.get('/add',  (req, res)=> {
     res.render('cupones-add');
   });
+
+router.post('/add', async (req, res)=> {
+    const{nombre,descuento,vecnimiento}=req.body;
+    const cupon={
+      nombre,
+      descuento,
+      vecnimiento
+    };
+   await pool.query('INSERT INTO cupones set ?',[cupon]);
+    // console.log(req.body);
+    res.send('enviados');
+  });
+
 
 module.exports=router;
