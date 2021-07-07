aecf30e102ce5893f8df64e1b327158c244c82f6
DESIT-DEV <desit.dev@gmail.com>
2021-07-06 21:43:44 -0600

    mando ya agregado el tema del admin

--- index.js
+++ index.js
@@ -1,47 +1,36 @@
-const express = require('express');
-const morgan= require('morgan');
-const path = require('path');
-
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const { helpers } = require("./lib/time");
 //inicios
 
-const app=express();
+const app = express();
 
 //config
-app.set('port',process.env.PORT || 4000);
+app.set("port", process.env.PORT || 4000);
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 //middleware
 
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
 //variables globales
-app.use((req,res,next)=>{
-    next();
+app.use((req, res, next) => {
+  next();
 });
+
+app.locals = helpers;
+
 //Rutas
-app.use(require('./routes/index'));
-app.use(require('./routes/auth'));
-app.use('/cupones',require('./routes/cupones'));
+app.use(require("./routes/index"));
+app.use(require("./routes/auth"));
+app.use("/cupones", require("./routes/cupones"));
 
 //public
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //server incia
 
-app.listen(app.get('port'),()=>{
-    
-});
-
-
-
-
-
-
-
-
-
-
-
+app.listen(app.get("port"), () => {});
